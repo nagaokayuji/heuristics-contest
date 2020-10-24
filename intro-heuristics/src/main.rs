6beab5da2b081951c680622d63ee5d907940e22c
Yuji Nagaoka <yvay5cqe@gmail.com>
2020-10-24 20:50:27 +0900

    add: greedy algorithm

--- intro-heuristics/src/main.rs
+++ intro-heuristics/src/main.rs
@@ -4,11 +4,18 @@ use {
     std::*,
     std::{cmp::*, collections::*, convert::*, iter::*, marker::*, mem::*, ops::*},
 };
+macro_rules ! chmin {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_min = min ! ($ ($ cmps ) ,+ ) ; if $ base > cmp_min {$ base = cmp_min ; true } else {false } } } ; }
+macro_rules ! chmax {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_max = max ! ($ ($ cmps ) ,+ ) ; if $ base < cmp_max {$ base = cmp_max ; true } else {false } } } ; }
+macro_rules ! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: min ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: min ($ a , min ! ($ ($ rest ) ,+ ) ) } } ; }
+macro_rules ! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
 fn main() {
-    input! {d:usize,c:[i64;26],s:[[i64;26];d],t:[Usize1;d]};
+    input! {d:usize,c:[i64;26],s:[[i64;26];d]};
 
     let mut input = Input { D: d, s: s, c: c };
-    calc_score(&input, &t);
+    let ans = solve_greedy(&input);
+    for &x in ans.iter() {
+        println!("{}", x + 1);
+    }
 }
 struct Input {
     D: usize,
@@ -28,6 +35,23 @@ fn calc_score(input: &Input, out: &Vec<usize>) -> i64 {
         score += input.s[d][out[d]];
     }
     score
+}
+fn solve_greedy(input: &Input) -> Vec<usize> {
+    let mut out = vec![];
+    for day in 0..input.D {
+        let mut max_score = -INF;
+        let mut best_i = 0usize;
+        for i in 0..26 {
+            out.push(i);
+            let score = calc_score(&input, &out);
+            if chmax!(max_score, score) {
+                best_i = i;
+            }
+            out.pop();
+        }
+        out.push(best_i);
+    }
+    out
 }
 
 fn sm(start: usize, end: usize) -> i64 {
