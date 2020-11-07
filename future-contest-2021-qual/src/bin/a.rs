b7a35350b383cd4faa537cb2aedd5aeaa4d391eb
Yuji Nagaoka <yvay5cqe@gmail.com>
2020-11-07 14:15:37 +0900

    add: struct

--- future-contest-2021-qual/src/bin/a.rs
+++ future-contest-2021-qual/src/bin/a.rs
@@ -6,5 +6,47 @@ use {
 };
 fn main() {
     input! {//
+        xy:[(usize,usize);100],
+    }
+    let mut input = Input { xy: xy };
+}
+struct Input {
+    xy: Vec<(usize, usize)>,
+}
+
+struct State {
+    pos: (usize, usize),
+    operations: Vec<char>,
+}
+struct Deque {
+    data: Vec<(usize, usize)>, // カードの数字, index
+    left: usize,
+    right: usize,
+}
+
+pub trait BinarySearch<T> {
+    fn lower_bound_by<F: Fn(&T) -> bool>(&self, f: F) -> usize;
+    fn lower_bound(&self, x: &T) -> usize;
+    fn upper_bound(&self, x: &T) -> usize;
+}
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound_by<F: Fn(&T) -> bool>(&self, f: F) -> usize {
+        let mut ng = -1;
+        let mut ok = self.len() as i64;
+        while (ok as i32 - ng as i32).abs() > 1 {
+            let mid = (ok + ng) / 2;
+            if f(&self[mid as usize]) {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+        }
+        ok as usize
+    }
+    fn lower_bound(&self, x: &T) -> usize {
+        self.lower_bound_by(|y| y >= x)
+    }
+    fn upper_bound(&self, x: &T) -> usize {
+        self.lower_bound_by(|y| y > x)
     }
 }
