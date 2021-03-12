69ec4cfb42a0424d4dbc48c48f6aaf4600cb5f4e
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-03-12 21:49:24 +0900

    add base fn

--- ahc001/src/bin/a.rs
+++ ahc001/src/bin/a.rs
@@ -9,27 +9,52 @@ macro_rules ! chmin {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let 
 macro_rules ! chmax {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_max = max ! ($ ($ cmps ) ,+ ) ; if $ base < cmp_max {$ base = cmp_max ; true } else {false } } } ; }
 macro_rules ! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: min ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: min ($ a , min ! ($ ($ rest ) ,+ ) ) } } ; }
 macro_rules ! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
-const LIMIT: f64 = 1.88;
+const LIMIT: f64 = 4.88;
 fn main() {
-    input!{
-        n:usize,
-        xyr: [(i64,i64,i64);n],
+    input! {
+        n: usize,
+        xyr: [(i64,i64,i64); n],
     }
+    get_time();
+    dbg!(get_time());
+    let input = Input { n: n, xyr: xyr };
+    solve(&input);
 }
+
+fn solve(input: &Input) {}
 /// 入力
 struct Input {
     n: usize,
-    xyr: [(i64,i64,i64);n],
+    xyr: Vec<(i64, i64, i64)>,
 }
 // 出力１個分
-struct Rect{
-    sx:i64,sy:i64,
-    ex:i64,ey:i64,
+#[derive(Clone, Copy)]
+struct Rect {
+    sx: i64,
+    sy: i64,
+    ex: i64,
+    ey: i64,
+}
+impl Rect {
+    fn area(&self) -> i64 {
+        // validation
+        assert!(self.sx < self.ex);
+        assert!(self.sy < self.ey);
+
+        //
+        return (self.ex - self.sx) * (self.ey - self.sy);
+    }
+}
+fn output(out: &[Rect]) {
+    for &rect in out.iter() {
+        println!("{} {} {} {}", rect.sx, rect.sy, rect.ex, rect.ey);
+    }
 }
 struct State {
+    out: Vec<Rect>,
 }
-impl State {
-}
+impl State {}
+fn calc_score(input: &Input, out: &Vec<Rect>) -> f64 {}
 
 /// 実行時間
 fn get_time() -> f64 {
